7e4c2124b2c0e323c4ddd93764219044b2bfbe33
aqasaz1928 <aqasaz1928@gmail.com>
2016-02-23 18:08:47 +0800

    3rd commit add week 1 task 3

---
+++ week_1/week_1_task_3/baidu_for_ie6/Resource/index.js
@@ -0,0 +1,16 @@
+﻿/**
+ * Created by Joker on 2015/10/17.
+ */
+//使用jquery使得输入框获取焦点时改变颜色
+$(document).ready(function () {
+    $("#wd").focus(function () {
+       $(".input-span").css({
+          "border":"1px solid #38f"
+       });
+    });
+    $("#wd").blur(function () {
+        $(".input-span").css({
+            "border":"1px solid #888"
+        });
+    });
+});

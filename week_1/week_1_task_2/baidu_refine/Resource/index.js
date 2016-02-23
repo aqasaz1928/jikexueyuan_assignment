919c8bdbea8a205f532e9e62a7738eefaf5d081d
aqasaz1928 <aqasaz1928@gmail.com>
2016-02-23 17:41:41 +0800

    commit when week_1 task_1 and task_2 has been appended

---
+++ week_1/week_1_task_2/baidu_refine/Resource/index.js
@@ -0,0 +1,11 @@
+﻿/**
+ * Created by Joker on 2015/10/17.
+ */
+//使用jquery使得输入框获取焦点时改变颜色
+$(document).ready(function () {
+    $("#wd").focus(function () {
+       $(".input-span").css({
+          "border":"1px solid #38f"
+       });
+    });
+});
